99c1a6af2f1859c256ead01a349f2a6cf470cce0
Mathieu Bongibault <mathjick8585@gmail.com>
2024-03-27 16:20:59 +0100

    patch for live test of RPM

--- Assets/Player/Weapon/TriggerModules/Marksman/TM_Marksman.cs
+++ Assets/Player/Weapon/TriggerModules/Marksman/TM_Marksman.cs
@@ -9,14 +9,8 @@ public class TM_Marksman : TriggerModule
     #endregion
     #region Variables
     private float timer = 0;
-    private float timeBeetweenShots = 0;
     private bool isHolding = false;
     #endregion
-
-    public void Start()
-    {
-        timeBeetweenShots = 60f / RPM;
-    }
 
     public void FixedUpdate()
     {
@@ -31,7 +25,7 @@ public class TM_Marksman : TriggerModule
         isHolding = true;
         if(timer <= 0)
         {
-            timer = timeBeetweenShots;
+            timer = 60f / RPM;
             Shoot();
         }
     }

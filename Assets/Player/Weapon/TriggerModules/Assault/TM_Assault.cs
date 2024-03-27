33ff92a4897c83c895c78c8d03ba65b0d166b703
Mathieu Bongibault <mathjick8585@gmail.com>
2024-03-27 16:19:39 +0100

    patch for live modification of assault

--- Assets/Player/Weapon/TriggerModules/Assault/TM_Assault.cs
+++ Assets/Player/Weapon/TriggerModules/Assault/TM_Assault.cs
@@ -9,14 +9,8 @@ public class TM_Assault : TriggerModule
     #endregion
     #region Variables
     private float timer = 0;
-    private float timeBeetweenShots = 0;
     private bool isHolding = false;
     #endregion
-
-    public void Start()
-    {
-        timeBeetweenShots = 60f / RPM;
-    }
 
     public void FixedUpdate()
     {
@@ -26,7 +20,7 @@ public class TM_Assault : TriggerModule
         }
         else if (isHolding)
         {
-            timer = timeBeetweenShots;
+            timer = 60f / RPM;
             Shoot();
         }
     }

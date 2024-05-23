ee2486e067f5af330baf7463bd2417eea0ddd172
Mathieu Bongibault <mathjick8585@gmail.com>
2024-05-24 00:18:24 +0200

    rewired bomber bot

--- Assets/Combat/Ennemies/MineEnnemies/MineBomberBot/MineBomber_IA.cs
+++ Assets/Combat/Ennemies/MineEnnemies/MineBomberBot/MineBomber_IA.cs
@@ -7,7 +7,7 @@ using UnityEngine.InputSystem.HID;
 
 public enum MineBomber_States
 {
-    Idle,Move,Attack
+    Idle,Move,Attack,Dead
 }
 public class MineBomber_IA : MonoBehaviour
 {
@@ -47,87 +47,107 @@ public class MineBomber_IA : MonoBehaviour
 
     public void Update()
     {
-        if (playerToFocus)
+        if(state != MineBomber_States.Dead)
         {
-            float distance = Vector3.Distance(playerToFocus.transform.position, this.transform.position);
-            if (distance < minAttackRange || distance > maxAttackRange)
+            if (playerToFocus)
             {
-                ChangeState(MineBomber_States.Move);
+                float distance = Vector3.Distance(playerToFocus.transform.position, this.transform.position);
+                if (distance < minAttackRange || distance > maxAttackRange)
+                {
+                    ChangeState(MineBomber_States.Move);
+                }
+                else
+                {
+                    ChangeState(MineBomber_States.Attack);
+                }
             }
             else
             {
-                ChangeState(MineBomber_States.Attack);
+                ChangeState(MineBomber_States.Idle);
             }
-        }
-        else
-        {
-            ChangeState(MineBomber_States.Idle);
         }
     }
 
     public void FixedUpdate()
     {
-        switch (this.state)
+        if (state != MineBomber_States.Dead)
         {
-            case MineBomber_States.Idle:
-                break;
-            case MineBomber_States.Move:
-                break;
-            case MineBomber_States.Attack:
-                if(attackTimer > 0) { attackTimer -= Time.deltaTime; }
-                Attack();
-                break;
+            switch (this.state)
+            {
+                case MineBomber_States.Idle:
+                    break;
+                case MineBomber_States.Move:
+                    break;
+                case MineBomber_States.Attack:
+                    if (attackTimer > 0) { attackTimer -= Time.deltaTime; }
+                    Attack();
+                    break;
+            }
         }
     }
 
     public void Move()
     {
-        float distance = Vector3.Distance(playerToFocus.transform.position, this.transform.position);
-        if (distance > maxAttackRange)
+        if (state != MineBomber_States.Dead)
         {
-            navMeshAgent.SetDestination(playerToFocus.transform.position);
-        }
-        if (distance < minAttackRange)
-        {
-            NavMeshHit hit;
-            Vector3 oppositeSide = transform.position - (playerToFocus.transform.position - transform.position);
-            NavMesh.SamplePosition(oppositeSide, out hit, 100, 1);
-            navMeshAgent.SetDestination(hit.position);
+            float distance = Vector3.Distance(playerToFocus.transform.position, this.transform.position);
+            if (distance > maxAttackRange)
+            {
+                navMeshAgent.SetDestination(playerToFocus.transform.position);
+            }
+            if (distance < minAttackRange)
+            {
+                NavMeshHit hit;
+                Vector3 oppositeSide = transform.position - (playerToFocus.transform.position - transform.position);
+                NavMesh.SamplePosition(oppositeSide, out hit, 100, 1);
+                navMeshAgent.SetDestination(hit.position);
+            }
         }
     }
+
     public void StopMoving()
     {
-        navMeshAgent.SetDestination(this.transform.position);
+        if (state != MineBomber_States.Dead)
+        {
+            navMeshAgent.SetDestination(this.transform.position);
+        }
     }
+
     public void Attack()
     {
-        //turn the ennemy to the player
-        Vector3 direction = playerToFocus.transform.position - this.transform.position;
-        direction.y = direction.y + 50;
-        this.transform.rotation = Quaternion.LookRotation(direction);
-        //shoot
-        if (attackTimer <= 0)
+        if (state != MineBomber_States.Dead)
         {
-            GameObject _newProjectile = Instantiate(projectileToShoot, projectileSpawnPoint.transform.position, canonAnchor.transform.rotation);
-            _newProjectile.GetComponent<Rigidbody>().AddForce(canonAnchor.transform.right * 1000);
-            attackTimer = attackRate;
+            //turn the ennemy to the player
+            Vector3 direction = playerToFocus.transform.position - this.transform.position;
+            direction.y = direction.y + 50;
+            this.transform.rotation = Quaternion.LookRotation(direction);
+            //shoot
+            if (attackTimer <= 0)
+            {
+                GameObject _newProjectile = Instantiate(projectileToShoot, projectileSpawnPoint.transform.position, canonAnchor.transform.rotation);
+                _newProjectile.GetComponent<Rigidbody>().AddForce(canonAnchor.transform.right * 1000);
+                attackTimer = attackRate;
+            }
         }
     }
 
     public void ChangeState(MineBomber_States _state)
     {
-        this.state = _state;
-        switch (_state)
+        if (state != MineBomber_States.Dead)
         {
-            case MineBomber_States.Idle:
-                StopMoving();
-                break;
-            case MineBomber_States.Move:
-                Move();
-                break;
-            case MineBomber_States.Attack:
-                StopMoving();
-                break;
+            this.state = _state;
+            switch (_state)
+            {
+                case MineBomber_States.Idle:
+                    StopMoving();
+                    break;
+                case MineBomber_States.Move:
+                    Move();
+                    break;
+                case MineBomber_States.Attack:
+                    StopMoving();
+                    break;
+            }
         }
     }
 }

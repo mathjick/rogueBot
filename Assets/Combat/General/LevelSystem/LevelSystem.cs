b1f55f3857586fc7d527d4aca889b3c6877c5331
Mathieu Bongibault <mathjick8585@gmail.com>
2024-09-05 16:35:55 +0200

    updated difficulty script + level scipt

--- Assets/Combat/General/LevelSystem/LevelSystem.cs
+++ Assets/Combat/General/LevelSystem/LevelSystem.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public struct LevelTreshold
+[System.Serializable]
+public class LevelTreshold
 {
     public int level;
     public int treshold;
+    public UnityEvent levelEvent;
 }
 public class LevelSystem : MonoBehaviour
 {
     public int exp;
+    public LevelTreshold currentLevel;
     public List<LevelTreshold> LevelTresholds;
 
     public int GetLevel()
@@ -28,5 +32,10 @@ public class LevelSystem : MonoBehaviour
     public void AddExp(int exp)
     {
         this.exp += exp;
+        if (GetLevel() > currentLevel.level)
+        {
+            currentLevel = LevelTresholds.Find(x => x.level == GetLevel());
+            currentLevel.levelEvent.Invoke();
+        }
     }
 }
--- Assets/GeneralScripts/DifficultySystem.cs
+++ Assets/GeneralScripts/DifficultySystem.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [System.Serializable]
-public struct Difficulty
+public class Difficulty
 {
     public float maxTime;
     public float difficulty;
@@ -12,11 +13,43 @@ public struct Difficulty
 public class DifficultySystem : MonoBehaviour
 {
     public float timer = 0;
-    public float difficulty = 1;
-    public Difficulty[] difficulties;
+    public Difficulty difficulty;
+    public List<Difficulty> difficulties;
+    public List<DifficultyCallBack> difficultyCallBacks;
+
+    public void Start()
+    {
+        difficulty = difficulties.First();
+        foreach (DifficultyCallBack d in FindObjectsOfType<DifficultyCallBack>())
+        {
+            d.SubscribeToDifficultySystem(this);
+        }
+    }
 
     public void Update()
     {
         timer += Time.deltaTime;
+        if (timer > difficulty.maxTime)
+        {
+            difficulty = getDifficulty();
+            foreach (DifficultyCallBack d in difficultyCallBacks)
+            {
+                d.UpdateDifficulty();
+            }
+        }
+    }
+
+    public Difficulty getDifficulty()
+    {
+        Difficulty _realDifficulty = difficulties.First();
+        foreach (Difficulty d in difficulties)
+        {
+            if (timer < d.maxTime)
+            {
+                _realDifficulty = d;
+                break;
+            }
+        }
+        return _realDifficulty;
     }
 }
